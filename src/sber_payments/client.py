7ef07fc16fc8fea560bac4258783ccf427dfe711
Anton Popov <ademaro@ya.ru>
2021-04-26 02:41:12 +0200

    Добавлен метод завершения двухстадийной оплаты

--- src/sber_payments/client.py
+++ src/sber_payments/client.py
@@ -101,3 +101,22 @@ class Client(object):
         # py3.9: self.params |= params
 
         return self._post(url, params={**self.params, **params})
+
+    def deposit(self, order_id: str, amount: int = 0):
+        """
+        Запрос завершения на полную сумму в деньгах (deposit.do)
+        https://securepayments.sberbank.ru/wiki/doku.php/integration:api:rest:requests:deposit
+
+        Для запроса завершения ранее пред авторизованного заказа используется запрос deposit.do.
+
+        :param order_id: Номер заказа в платежной системе. Уникален в пределах системы. Отсутствует если регистрация
+                         заказа на удалась по причине ошибки, детализированной в ErrorCode.
+        :param amount: Сумма возврата в минимальных единицах валюты (в копейках или центах).
+                       Внимание! Если в этом параметре указать 0, завершение произойдёт на всю предавторизованную сумму.
+        """
+        url = f"{self.URL}deposit.do"
+        params = {
+            'orderId': order_id,
+            'amount': amount,
+        }
+        return self._post(url, params={**self.params, **params})
